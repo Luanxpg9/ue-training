34522bd51befa1c9f8063c8499e85514467909be
Luan Barbosa <luanxpg9@gmail.com>
2021-05-01 17:15:59 -0300

    logical operations

--- module2/tripleX.cpp
+++ module2/tripleX.cpp
@@ -3,7 +3,18 @@
 int main()
 {
     std::cout << "Try solving this little puzzle, little fella\n";
-    std::cout << std::endl;
     std::cout << "  or are you too smol brain for it? ...";
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << '\n'
+              << product;
+
     return 0;
 }
