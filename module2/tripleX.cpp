8aa16bff42f64ce0727c143f9cc6150ea674c36c
Luan Barbosa <luanxpg9@gmail.com>
2021-05-01 17:32:28 -0300

    added some comments to clarify the code

--- module2/tripleX.cpp
+++ module2/tripleX.cpp
@@ -2,19 +2,23 @@
 
 int main()
 {
+    //Print welcome messages to the terminal
     std::cout << "Try solving this little puzzle, little fella\n";
     std::cout << "  or are you too smol brain for it? ...";
 
+    // Declare variables
     int a = 4;
     int b = 3;
     int c = 2;
 
+    // Set up the game logic
     int sum = a + b + c;
     int product = a * b * c;
 
+    // Show the numbers logic
     std::cout << std::endl;
     std::cout << sum << '\n'
-              << product;
+              << product << std::endl;
 
     return 0;
 }
