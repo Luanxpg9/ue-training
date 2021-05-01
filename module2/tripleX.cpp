a547e50dd30bfa97cbdfd51d3e3bf0e9252264af
Luan Barbosa <luanxpg9@gmail.com>
2021-05-01 02:59:53 -0300

    line showing how to <br> in cpp

--- module2/tripleX.cpp
+++ module2/tripleX.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     std::cout << "Try solving this little puzzle, little fella\n";
+    std::cout << std::endl;
     std::cout << "  or are you too smol brain for it? ...";
     return 0;
 }
